ead03154ba4a8605a781d163a1c9bc559f00855d
gbpaixao-maxxi <125409208+gbpaixao-maxxi@users.noreply.github.com>
2023-02-17 14:30:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import GreenButton from './components/GreenButton'
@@ -7,12 +7,6 @@ import FeatureToggle from '../feature-toggle/FeatureToggle'
 function App() {
   const [count, setCount] = useState(0)
   const featureToggle = new FeatureToggle()
-  
-  useEffect(() => {
-    console.log(featureToggle.isEnabledForUser('featureDeUmUser'))
-    console.log(featureToggle.isEnabledForGroup('featureParaMultiplosUsers'))
-    console.log(featureToggle.isEnabledForAdmins('featureParaAdmins'))
-  },[])
   
   return (
     <div className="App">
